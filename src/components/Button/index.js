45d9daf80aa48d48099b8f5f04e1e4907aae64bd
Aneesh <brainchildofbalan@gmail.com>
2024-07-21 14:31:15 +0530

    added new component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,1 +1,11 @@
-export { default } from './button'
+import React from 'react'
+import classes from './button.module.scss'
+const Button = ({title, type = 'primary'}) => {
+  return (
+    <button className={classes[type]}>
+        <span>{title}</span>
+    </button>
+  )
+}
+
+export default Button
